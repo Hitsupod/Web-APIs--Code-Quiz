ba71730693465c0da45a15bbfabe65cde21a3a0c
Reggie Littleton <Hitsupod@gmail.com>
2020-05-07 00:19:51 -0500

    Sudo Code Written

---
+++ script.js
@@ -0,0 +1,32 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// (Create A start Button with a click event)
+
+// THEN a timer starts and I am presented with a question 
+// Create a timer
+// Create Variable to hold Score 
+
+// Link Event to start timer
+
+// Create Alert that generates first question
+
+// WHEN I answer a question 
+// Choice between true and false would be easy 
+
+
+// THEN I am presented with another question
+// Prompts a new question 
+
+
+// WHEN I answer a question incorrectly (If false --) 
+// When I answer a question correctly (If true ++)
+// THEN time is subtracted from the clock (-- from timer)
+
+// WHEN all questions are answered or the timer reaches 0 
+// If clock reach 0 end game
+// Once all questions are answered end game 
+//THEN the game is over
+
+// WHEN the game is over
+// THEN I can save my initials and score (Input any 3 characters)
+// Display section for both score and intials displays 
