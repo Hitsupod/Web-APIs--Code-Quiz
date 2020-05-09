1040e52c361aeb201fe48a04b0ba7f48eab353bc
Reggie Littleton <Hitsupod@gmail.com>
2020-05-09 17:30:58 -0500

    New updates to style

--- script.js
+++ script.js
@@ -1,27 +1,111 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// (Create A start Button )
-// Quiz points 
-const quizCo = document.getElementById('hold');
-const resultsCo = document.getElementById('results');
-// const submitButton = document.getElementById('Okay');
-var startEl = document.querySelector('#start');
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionsContainerEl = document.getElementById('questionsCon');
 
-/* Click event for the start button 
-startEl.addEventListener("click", function() {
-    alert('Lets Begin!')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+var score = 0;
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener('click',startGame);
+
+function startGame() {
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    questionsContainerEl.classList.remove('hide');
+    currentQuestionsIndex = 0;
+    nextQuestion();
+    
+}
+
+
+
+function nextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+    
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+        questions.answers.forEach(answer => {
+            const button = document.createElement('button')
+            button.innerText = answer.text
+            button.classList.add('btn')
+            if (answer.correct) {
+                button.dataset.correct = answer.correct
+                score ++;
+            }
+            button.addEventListener('click', selectAnswer)
+            answerButtonsElement.appendChild(button)
+        })
+    }
+
+
+function selectAnswer() {
+    resetState()
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+
+
+// Questions 
+const questions = [
+    {
+      question: 'Which is a function ?',
+      answers: [
+        { text: 'funcion ()', correct: true },
+        { text: 'method ()', correct: false },
+        { text: 'variable ()', correct: false },
+        { text: 'class ()', correct: false }
+      ]
+    },
+    {
+      question: 'Who made this test',
+      answers: [
+        { text: 'Reggie', correct: true },
+        { text: 'The professor', correct: false },
+        { text: 'Some rando on stack overflow ', correct: false },
+        { text: 'Some student I paid', correct: false }
+      ]
+    },
+    {
+      question: 'Best coffee?',
+      answers: [
+        { text: 'Latte', correct: false },
+        { text: 'Black', correct: true },
+        { text: 'Espresso', correct: false },
+        { text: 'Mocha', correct: false }
+      ]
+    },
+    {
+      question: 'Best Ice Cream?',
+      answers: [
+        { text: 'Chocolate', correct: false },
+        { text: 'Cookies and Cream', correct: true },
+        { text: 'Mint Chocolate Chip', correct: false },
+        { text: 'Strawberry', correct: false }
+      ]
+    }
+  ]
+
+  // Timer 
+var startTime = document.querySelector('start-btn');
+startTime.addEventListener("click", function() {
     var fiveMinutes = 60 * 2,
         display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-    confirm('Are you enjoying class?');
-    confirm('Would You recommend to anyone?')
+    startTimer(twoMinutes, display);
   });
-// THEN a timer starts and I am presented with a question 
-// Create a timer
-*/
-// Create Variable to hold Score 
-var score = 0;
-// Link Event to start timer 
+
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
@@ -38,138 +122,3 @@ function startTimer(duration, display) {
         }
     }, 1000);
 }
-
-
-// WHEN I answer a question 
-// Choice between true and false would be easy 
-
-
-// THEN I am presented with another question
-// Prompts a new question 
-
-
-// WHEN I answer a question incorrectly (If false --) 
-// When I answer a question correctly (If true ++)
-// THEN time is subtracted from the clock (-- from timer)
-
-// WHEN all questions are answered or the timer reaches 0 
-// If clock reach 0 end game
-// Once all questions are answered end game 
-//THEN the game is over
-
-// WHEN the game is over
-// THEN I can save my initials and score (Input any 3 characters)
-// Display section for both score and intials displays 
-
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
-
-let shuffledQuestions, currentQuestionIndex
-
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
-
-function startGame() {
-  startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
-}
-
-function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
-    }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
-}
-
-function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-  }
-}
-
-function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
-  } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
-  }
-}
-
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
-  } else {
-    element.classList.add('wrong')
-  }
-}
-
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
-}
-
-const questions = [
-  {
-    question: 'What is 2 + 2?',
-    answers: [
-      { text: '4', correct: true },
-      { text: '22', correct: false }
-    ]
-  },
-  {
-    question: 'Who is the best YouTuber?',
-    answers: [
-      { text: 'Web Dev Simplified', correct: true },
-      { text: 'Traversy Media', correct: true },
-      { text: 'Dev Ed', correct: true },
-      { text: 'Fun Fun Function', correct: true }
-    ]
-  },
-  {
-    question: 'Is web development fun?',
-    answers: [
-      { text: 'Kinda', correct: false },
-      { text: 'YES!!!', correct: true },
-      { text: 'Um no', correct: false },
-      { text: 'IDK', correct: false }
-    ]
-  },
-  {
-    question: 'What is 4 * 2?',
-    answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
-    ]
-  }
-]
