90699d099a4f7e83707f10b489492542c3e1e941
Reggie Littleton <Hitsupod@gmail.com>
2020-05-09 21:07:42 -0500

    Minor Changes

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
 const questionsContainerEl = document.getElementById('questionsCon');
 
-const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
@@ -10,102 +9,48 @@ var score = 0;
 
 let shuffledQuestions, currentQuestionIndex;
 
-startButton.addEventListener('click',startGame);
-
-function startGame() {
-    startButton.classList.add('hide');
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-    questionsContainerEl.classList.remove('hide');
-    currentQuestionsIndex = 0;
-    nextQuestion();
-    
-}
-
-
-
-function nextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-    
-}
-
-function showQuestion(question) {
-    questionElement.innerText = question.question;
-        questions.answers.forEach(answer => {
-            const button = document.createElement('button')
-            button.innerText = answer.text
-            button.classList.add('btn')
-            if (answer.correct) {
-                button.dataset.correct = answer.correct
-                score ++;
-            }
-            button.addEventListener('click', selectAnswer)
-            answerButtonsElement.appendChild(button)
-        })
-    }
-
-
-function selectAnswer() {
-    resetState()
-}
-
-function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    }
-}
-
-
-
-// Questions 
 const questions = [
     {
-      question: 'Which is a function ?',
-      answers: [
+        question: 'Which is a function ?',
+        answers: [
         { text: 'funcion ()', correct: true },
         { text: 'method ()', correct: false },
         { text: 'variable ()', correct: false },
         { text: 'class ()', correct: false }
-      ]
+        ]
     },
     {
-      question: 'Who made this test',
-      answers: [
+        question: 'Who made this test',
+        answers: [
         { text: 'Reggie', correct: true },
         { text: 'The professor', correct: false },
         { text: 'Some rando on stack overflow ', correct: false },
         { text: 'Some student I paid', correct: false }
-      ]
+        ]
     },
     {
-      question: 'Best coffee?',
-      answers: [
+        question: 'Best coffee?',
+        answers: [
         { text: 'Latte', correct: false },
         { text: 'Black', correct: true },
         { text: 'Espresso', correct: false },
         { text: 'Mocha', correct: false }
-      ]
+        ]
     },
     {
-      question: 'Best Ice Cream?',
-      answers: [
+        question: 'Best Ice Cream?',
+        answers: [
         { text: 'Chocolate', correct: false },
         { text: 'Cookies and Cream', correct: true },
         { text: 'Mint Chocolate Chip', correct: false },
         { text: 'Strawberry', correct: false }
-      ]
+        ]
     }
-  ]
+]
 
-  // Timer 
-var startTime = document.querySelector('start-btn');
-startTime.addEventListener("click", function() {
-    var fiveMinutes = 60 * 2,
-        display = document.querySelector('#time');
-    startTimer(twoMinutes, display);
-  });
+startButton.addEventListener('click',startGame);
 
+// Timer 
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
@@ -120,5 +65,68 @@ function startTimer(duration, display) {
         if (--timer < 0) {
             timer = duration;
         }
+        if(seconds === 0) {
+            alert('Game Over!')
+        }
     }, 1000);
+}
+
+startButton.addEventListener("click", function() {
+    var twoMinutes = 60 * 2,
+        display = document.querySelector('#time');
+    startTimer(twoMinutes, display);
+});
+
+// Start The Questions 
+function startGame() {
+    
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    questionsContainerEl.classList.remove('hide');
+    currentQuestionsIndex = 0;
+    nextQuestion();
+    nextButton.classList.remove('hide')
+
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsElement.appendChild(button)
+    })
+}
+
+
+// Reset 
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+// Test Loop
+function questionsloop(questions) {
+
+    for (var i = 0; i < questions.length; i ++);
+    questions.innerText('#question');
+    if (answer.correct) {
+        button.dataset.correct = answer.correct
+        score ++;
+    }
+    console.log(questions)
+}
+
+// Next Questions 
+function nextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+    
 }
